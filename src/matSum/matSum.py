34f0ef95db76d778bf754379001fb27dbd0a49ba
marcosamaris <marcos.amaris@gmail.com>
2016-02-18 10:52:33 -0200

    Matrix multiplication over Tesla K40

--- src/matSum/matSum.py
+++ src/matSum/matSum.py
@@ -9,6 +9,6 @@ subprocess.check_output("rm -f *.csv",  shell = True)
 
 programs = ["matrix_sum_normal", "matrix_sum_coalesced"]
 
-parameters = ["512 0", "1024 0","2048 0","4096 0","8192 0","16384 0"]
+parameters = ["256 16 0", "512 16 0", "1024 16 0","2048 16 0","4096 16 0","8192 16 0","16384 16 0"]
 
 common.run_traces(programs, parameters)
--- src/sorting/sorting.py
+++ src/sorting/sorting.py
@@ -13,7 +13,7 @@ parameters = ["2048 0","4096 0","8192 0","16384 0","32768 0","65536 0"]
 
 common.run_traces(programs, parameters)
 
-programs = ["bitonic_sort"]
+programs = ["bitonic"]
 
 parameters = ["32768 0","65536 0", "131072 0", "262144 0", "524288 0" ,"1048576 0" ,"2097152 0" ,"4194304 0"]
 
