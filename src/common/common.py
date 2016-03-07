85061cca7b5cb35e2719478674cca87b617b3126
marcosamaris <marcos.amaris@gmail.com>
2016-03-07 23:52:49 +0100

    Modified the file common.py to add the size of the experiments

--- src/common/common.py
+++ src/common/common.py
@@ -6,11 +6,12 @@ import os
 import subprocess
 import argparse
 
-gpu = "Tesla"                    
+gpu = "Tesla"
+experiment = 1                    
 
 def run_traces(programs, parameters, kernel, traces):
 
-    for i in range(0, 10):
+    for i in range(0, experiment):
         os.system("mkdir -p ../logs/run_" + str(i))    
         for trace in traces:
             for program in programs:
@@ -23,7 +24,7 @@ def run_traces(programs, parameters, kernel, traces):
                     if trace == " ":
                         cmd += trace 
                         cmd += " ./" + program + " " + param 
-                        cmd += " 2> Temp; cat Temp | tail -n +4 >>  ../logs/run_" + str(i) + "/"  + program + "-traces.csv"
+                        cmd += " 2> Temp; cat Temp | awk '{print "+ param + "\",\" $0}'" | tail -n +4 >>  ../logs/run_" + str(i) + "/"  + program + "-traces.csv"
                         output = subprocess.check_output(cmd,  shell = True)
                         output = subprocess.check_output("cat ../logs/run_" + str(i) + "/" + program + "-traces.csv | grep " + kernel + " > ../logs/run_" + str(i) + "/" + program + "-kernel-traces.csv", shell = True)
                         output = subprocess.check_output("cat ../logs/run_" + str(i) + "/" + program + "-traces.csv | grep HtoD > ../logs/run_" + str(i) + "/" + program + "-HtoD-traces.csv", shell = True)
@@ -31,12 +32,11 @@ def run_traces(programs, parameters, kernel, traces):
                     elif trace == "--metrics all":
                         cmd += trace 
                         cmd += " ./" + program + " " + param 
-                        cmd += " 2> Temp; cat Temp | grep '" + gpu + "' >> ../logs/run_" + str(i) + "/" + program + "-metrics.csv"
+                        cmd += " 2> Temp; cat Temp | awk '{print "+ param + "\",\" $0}'" | grep '" + gpu + "' >> ../logs/run_" + str(i) + "/" + program + "-metrics.csv"
                         output = subprocess.check_output(cmd,  shell = True)#, stderr=subprocess.STDOUT)
                     elif trace == "--events all":
                         cmd += trace
                         cmd += " ./" + program + " " + param 
-                        cmd += " 2> Temp; cat Temp | grep '" + gpu + "' >> ../logs/run_" + str(i) + "/" + program + "-events.csv"
-                        output = subprocess.check_output(cmd,  shell = True)#, stderr=subprocess.STDOUT)
+                        cmd += " 2> Temp; cat Temp | awk '{print "+ param + "\",\" $0}'" | grep '" + gpu + "' >> ../logs/run_" + str(i) + "/" + program + "-events.csv"
+                        output = subprocess.check_output(cmd,  shell = True)
 
-                    
