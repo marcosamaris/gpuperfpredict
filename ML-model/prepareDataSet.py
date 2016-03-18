2dffcc9bb24e394477415db7b1813cfe40bb1832
dyabmo <muhamed.dyab@gmail.com>
2016-03-18 18:25:35 +0100

    Initial version

--- ML-model/prepareDataSet.py
+++ ML-model/prepareDataSet.py
@@ -4,10 +4,10 @@ import matplotlib.pyplot as plt
 
 from pandas import DataFrame
 
-#PLAN: get data of 1 GPU first with and without L1 cache
+#PLAN: get data of 1 GPU first with and without L1 cache compute 3 capability
+#Add GPU data manually 
 
 # to control which GPU data to use
-
 gpus = pd.Series(["Tesla-K40","Tesla-k40-UsingL1"]);
 
 #Contians folder paths for GPUs
@@ -17,10 +17,19 @@ gpuPath=pd.Series([""]);
 for i in range(0,gpus.size):
 	gpuPath[i] = "../data/"+gpus[i];
 
-print(gpuPath);
+#print(gpuPath);
 
-with open(fname) as f:
-    content = f.readlines()
+#deviceQueryPath=gpuPath[0] + gpus
+#with open(fname) as f:
+#    content = f.readlines()
+
+deviceQueryDF = pd.DataFrame({ 'gpu_name' : pd.Categorical(["Tesla-K40","Tesla-k40-UsingL1"]),
+			       'compute_version': np.array([3,3]),
+			       'num_of_cores': np.array([2880,2880],dtype='int32'),
+	                       'max_clock_rate' : np.array([745,745],dtype='int32'), #in Mhz
+			       'l1_cache_used': np.array([0,1])})
+
+print(deviceQueryDF);
 
 #create a dataframe with zero rows, just the colums labels
 eventsDF = pd.read_csv("../data/eventsNames-3X.csv");
