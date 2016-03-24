7200898a1799b6ca8be8e860922fec97d99c7307
dyabmo <muhamed.dyab@gmail.com>
2016-03-24 18:21:04 +0100

    Modified to generate all traces in one file

--- ML-model/prepareDataSet.py
+++ ML-model/prepareDataSet.py
@@ -1,48 +1,61 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+import os
 
 from pandas import DataFrame
 
 #PLAN: get data of 1 GPU first with and without L1 cache compute 3 capability
-#Add GPU data manually 
+#Add GPU data manually ( automatically would be too headache for its efficiency )
+
+#Compute Version
+computeVersion = 3
 
 # to control which GPU data to use
 gpus = pd.Series(["Tesla-K40","Tesla-k40-UsingL1"]);
 
-#Contians folder paths for GPUs
-gpuPath=pd.Series([""]);
-
-#Construct folder paths
-for i in range(0,gpus.size):
-	gpuPath[i] = "../data/"+gpus[i];
-
-#print(gpuPath);
+#List of applications( to be continued)
+applications = pd.Series(["bitonic","dotProd","matMul_gpu","matMul_gpu_sharedmem","matMul_gpu_sharedmem_uncoalesced",
+                          "matMul_gpu_uncoalesced","matrix_sum_coalesced","matrix_sum_normal","quicksort"]);
 
 #deviceQueryPath=gpuPath[0] + gpus
 #with open(fname) as f:
 #    content = f.readlines()
 
-deviceQueryDF = pd.DataFrame({ 'gpu_name' : pd.Categorical(["Tesla-K40","Tesla-k40-UsingL1"]),
-			       'compute_version': np.array([3,3]),
+deviceQueryDF = pd.DataFrame({ 'gpu_name' : gpus,
+			       'compute_version': np.array([computeVersion,computeVersion]),
 			       'num_of_cores': np.array([2880,2880],dtype='int32'),
 	                       'max_clock_rate' : np.array([745,745],dtype='int32'), #in Mhz
 			       'l1_cache_used': np.array([0,1])})
 
-print(deviceQueryDF);
+#print(deviceQueryDF);
 
 #create a dataframe with zero rows, just the colums labels
-eventsDF = pd.read_csv("../data/eventsNames-3X.csv");
+eventsHeaderDF = pd.read_csv("../data/eventsNames-3X.csv");
 
-metricsDF = pd.read_csv("../data/metricsNames-3X.csv");
+metricsHeaderDF = pd.read_csv("../data/metricsNames-3X.csv");
 
-tracesDF = pd.read_csv("../data/tracesNames-3X.csv");
+tracesHeaderDF = pd.read_csv("../data/tracesNames-3X.csv");
 
-#to access columns:
-#df.columns
-#df.columns[0] ...
+#Contians folder paths for GPUs
+gpuPath=pd.Series([""]);
 
-#to list column types: df.dtypes
+## Get traces data 
+#Construct folder paths and search for traces in each GPU directory
+for i in range(0,gpus.size):
+	gpuPath[i] = "../data/"+gpus[i]+"/run_0/";
+	#print(gpuPath);
 
-
+	for file in os.listdir(gpuPath[i]):
+    		if file.endswith("-kernel-traces.csv"):
+			fullPath = gpuPath[i] + file;
+        		print(fullPath);
+			tempDF = pd.read_csv(fullPath,header=None, names = tracesHeaderDF.columns );
+			#print(tempDF);
+			tracesHeaderDF = tracesHeaderDF.append(tempDF);
+			#print(tracesHeaderDF);
+			
+#print(tracesHeaderDF);
+tracesHeaderDF.to_csv("traces.csv");
+#about 6000 samples, due to bitonic samples
 
