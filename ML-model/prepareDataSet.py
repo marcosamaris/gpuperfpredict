5b6857c97e410f584e31061961e8741c829f349c
dyabmo <muhamed.dyab@gmail.com>
2016-03-25 16:40:39 +0100

    Each of traces, metrics and events is in one file

--- ML-model/prepareDataSet.py
+++ ML-model/prepareDataSet.py
@@ -28,7 +28,7 @@ deviceQueryDF = pd.DataFrame({ 'gpu_name' : gpus,
 	                       'max_clock_rate' : np.array([745,745],dtype='int32'), #in Mhz
 			       'l1_cache_used': np.array([0,1])})
 
-#print(deviceQueryDF);
+deviceQueryDF.to_csv('deviceData.csv');
 
 #create a dataframe with zero rows, just the colums labels
 eventsHeaderDF = pd.read_csv("../data/eventsNames-3X.csv");
@@ -40,7 +40,7 @@ tracesHeaderDF = pd.read_csv("../data/tracesNames-3X.csv");
 #Contians folder paths for GPUs
 gpuPath=pd.Series([""]);
 
-## Get traces data 
+## Get traces data: need to extract running time .. 
 #Construct folder paths and search for traces in each GPU directory
 for i in range(0,gpus.size):
 	gpuPath[i] = "../data/"+gpus[i]+"/run_0/";
@@ -51,11 +51,51 @@ for i in range(0,gpus.size):
 			fullPath = gpuPath[i] + file;
         		print(fullPath);
 			tempDF = pd.read_csv(fullPath,header=None, names = tracesHeaderDF.columns );
-			#print(tempDF);
 			tracesHeaderDF = tracesHeaderDF.append(tempDF);
-			#print(tracesHeaderDF);
-			
-#print(tracesHeaderDF);
+
+#Select only duration
+tracesHeaderDF = tracesHeaderDF[['Duration']];
+#tracesHeaderDF = tracesHeaderDF.ix[:,'Duration':'Duration']
 tracesHeaderDF.to_csv("traces.csv");
 #about 6000 samples, due to bitonic samples
+
+#Get events data
+#What do I need from events exactly ?
+for i in range(0,gpus.size):
+	gpuPath[i] = "../data/"+gpus[i]+"/run_0/";
+
+	for file in os.listdir(gpuPath[i]):
+    		if file.endswith("-events.csv"):
+			fullPath = gpuPath[i] + file;
+        		print(fullPath);
+			tempDF = pd.read_csv(fullPath,header=None, names = eventsHeaderDF.columns );
+			#print(tempDF);
+			eventsHeaderDF = eventsHeaderDF.append(tempDF);
+			
+eventsHeaderDF.to_csv("events.csv");
+
+
+#Get Metrics data
+#What do I need from metrics exactly ?
+for i in range(0,gpus.size):
+	gpuPath[i] = "../data/"+gpus[i]+"/run_0/";
+
+	for file in os.listdir(gpuPath[i]):
+    		if file.endswith("-metrics.csv"):
+			fullPath = gpuPath[i] + file;
+        		print(fullPath);
+			tempDF = pd.read_csv(fullPath,header=None, names = metricsHeaderDF.columns );
+			#print(tempDF);
+			metricsHeaderDF = metricsHeaderDF.append(tempDF);
+			
+metricsHeaderDF.to_csv("metrics.csv");
+
+#Metrics.csv, events.csv and traces.csv are different in number of samples ... should be equal
+# exclude bitonic for now ?
+#How to get number of threads ? is it from traces.csv ? gridX * gridY * gridZ * blockX * blockY * blockZ ?
+
+##################################################
+# To create dummy last dataframe
+##################################################
+
 
