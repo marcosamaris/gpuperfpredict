8f8d25fc4a6642a0d0361257352485456d55b865
dyabmo <muhamed.dyab@gmail.com>
2016-04-18 18:47:29 +0200

    Calculated error value, Added other regression models for comparison

--- ML-model/prediction.py
+++ ML-model/prediction.py
@@ -2,9 +2,15 @@ import numpy as np
 from sklearn import datasets
 from sklearn import linear_model
 from sklearn import preprocessing
+from sklearn.metrics import mean_absolute_error
+from sklearn.metrics import mean_squared_error
+from sklearn.metrics import r2_score
+from sklearn import svm
 
-#TODO: feature normalization
-#TODO: negative predicted values ?
+#TODO: check negatively predicted values
+#TODO: use different linear models
+
+#========================Preparing the data===================================#
 
 #Import all dataset
 dataset = np.genfromtxt('datasetDF.csv', dtype=float, delimiter=',', skip_header =1);
@@ -22,22 +28,77 @@ trainingSetCount = int(80 * samplesCount /100);
 X = dataset[0:trainingSetCount,0:featuresCount]; # last one not included
 y = dataset[0:trainingSetCount,outputIndex];
 
+#True Output values that will be used in calcuating the accuracy of prediction
+y_true = dataset[trainingSetCount+1:samplesCount,outputIndex]
+print y_true
+
 #Scale values with mean = zero and standard deviation =1
 std_scale = preprocessing.StandardScaler().fit(X)
 X_std = std_scale.transform(X)
-print X_std;
-
-
-#Training phase
-lr = linear_model.LinearRegression();
-lr.fit(X_std,y);
+#print X_std;
 
 #Scale test set
 X_val = dataset[trainingSetCount+1:samplesCount,0:featuresCount];
 X_val_std = std_scale.transform(X_val)
-print X_val_std;
+#print X_val_std;
+
+#=====================Ordinary Least Squares Linear model======================================
+print "=====================Ordinary Least Squares================"
+#Training phase
+lr = linear_model.LinearRegression();
+lr.fit(X_std,y);
 
 #Prediction for test set
-print lr.predict(X_val_std)
+y_pred = lr.predict(X_val_std)
+#print y_pred
+
+#Calculating prediction error
+error = mean_absolute_error(y_true,y_pred);
+print "Mean absolute error: " + str(error) + ", best is zero";
+
+error = mean_squared_error(y_true, y_pred) 
+print "Mean squared error: " + str(error) + ", best is zero";
+
+error = r2_score(y_true, y_pred)
+print "R^2 Error: " + str(error) + ", best is 1.0";
+
+#===================Ridge Regression==========================================
+print "=======================Ridge Regression=================="
+#Try different regularization parameters
+ridgeCV = linear_model.RidgeCV(alphas=[0.01,0.1,0.3,0.6, 1.0,3.0,6.0, 10.0])
+
+ridgeCV.fit(X_std,y);
+print "Used alpha: " + str(ridgeCV.alpha_);
+
+y_pred = ridgeCV.predict(X_val_std);
+
+#Calculating prediction error
+error = mean_absolute_error(y_true,y_pred);
+print "Mean absolute error: " + str(error) + ", best is zero";
+
+error = mean_squared_error(y_true, y_pred) 
+print "Mean squared error: " + str(error) + ", best is zero";
+
+error = r2_score(y_true, y_pred)
+print "R^2 Error: " + str(error) + ", best is 1.0";
+
+#=======================Support Vector Regression=============================
+print "================Support Vector Regression===================="
+svmR = svm.SVR();
+
+svmR.fit(X_std,y);
+
+y_pred = svmR.predict(X_val_std);
+#print y_pred
+#Calculating prediction error
+error = mean_absolute_error(y_true,y_pred);
+print "Mean absolute error: " + str(error) + ", best is zero";
+
+error = mean_squared_error(y_true, y_pred) 
+print "Mean squared error: " + str(error) + ", best is zero";
+
+error = r2_score(y_true, y_pred)
+print "R^2 Error: " + str(error) + ", best is 1.0";
+
 
 
