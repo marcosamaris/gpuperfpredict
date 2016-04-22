1f01e26a399dc2fb2d40838854f8a9073af630d6
dyabmo <muhamed.dyab@gmail.com>
2016-04-22 20:16:13 +0200

    Added Elastic Net model, calculated training error

--- ML-model/prediction.py
+++ ML-model/prediction.py
@@ -6,11 +6,15 @@ from sklearn.metrics import mean_absolute_error
 from sklearn.metrics import mean_squared_error
 from sklearn.metrics import r2_score
 from sklearn import svm
+import logging
 
 #TODO: check negatively predicted values
 #TODO: use different linear models
 #TODO: for comparison between different ML models use mean square error, for comparison with analytical model use accuracy: mean(predicted/true value * 100)
 
+logging.basicConfig(filename='prediction_results.log',filemode='w',level=logging.WARNING)
+
+#add matmul gpu uncoalased ..
 #========================Preparing the data===================================#
 
 #Import all dataset
@@ -25,7 +29,10 @@ featuresCount = dataset.shape[1]-1;
 outputIndex = dataset.shape[1]-1;
 
 trainingSetCount = int(80 * samplesCount /100);
+#If I needed to overwrite it
+trainingSetCount = 530;
 
+print trainingSetCount
 #For training set: separate the feature set from the target attributes
 X = dataset[featureStart:trainingSetCount,featureStart:featuresCount]; # last one not included
 y = dataset[featureStart:trainingSetCount,outputIndex];
@@ -50,6 +57,12 @@ print "=====================Ordinary Least Squares================"
 lr = linear_model.LinearRegression();
 lr.fit(X_std,y);
 
+training_error = lr.score(X_std,y);
+print "Training error = " + str(training_error) + ", best is 1.0";
+
+test_error = lr.score(X_val_std,y_true );
+print "Test error = " + str(test_error) + ", best is 1.0";
+
 #Prediction for test set
 y_pred = lr.predict(X_val_std)
 #print y_pred
@@ -61,9 +74,8 @@ print "Mean absolute error: " + str(error) + ", best is zero";
 error = mean_squared_error(y_true, y_pred) 
 print "Mean squared error: " + str(error) + ", best is zero";
 
-error = r2_score(y_true, y_pred)
-print "R^2 Error: " + str(error) + ", best is 1.0";
-
+#accuracy = np.mean( np.divide(y_pred,y_true) )
+#print accuracy
 #===================Ridge Regression==========================================
 print "=======================Ridge Regression=================="
 #Try different regularization parameters
@@ -71,6 +83,12 @@ ridgeCV = linear_model.RidgeCV(alphas=[0.01,0.1,0.3,0.6, 1.0,3.0,6.0, 10.0])
 
 ridgeCV.fit(X_std,y);
 print "Used alpha: " + str(ridgeCV.alpha_);
+
+training_error = ridgeCV.score(X_std,y);
+print "Training error = " + str(training_error) + ", best is 1.0";
+
+test_error = ridgeCV.score(X_val_std,y_true );
+print "Test error = " + str(test_error) + ", best is 1.0";
 
 y_pred = ridgeCV.predict(X_val_std);
 
@@ -81,14 +99,70 @@ print "Mean absolute error: " + str(error) + ", best is zero";
 error = mean_squared_error(y_true, y_pred) 
 print "Mean squared error: " + str(error) + ", best is zero";
 
-error = r2_score(y_true, y_pred)
-print "R^2 Error: " + str(error) + ", best is 1.0";
+#accuracy = np.mean( np.divide(y_pred,y_true) )
+#print accuracy
+
+
+#=========================LASSO ==============================
+
+print "=======================LASSO Regression=================="
+#Try different regularization parameters
+lassoCV = linear_model.LassoCV(alphas=[0.01,0.1,0.3,0.6, 1.0,3.0,6.0, 10.0])
+
+lassoCV.fit(X_std,y);
+print "Used alpha: " + str(lassoCV.alpha_);
+
+training_error = lassoCV.score(X_std,y);
+print "Training error = " + str(training_error) + ", best is 1.0";
+
+test_error = lassoCV.score(X_val_std,y_true );
+print "Test error = " + str(test_error) + ", best is 1.0";
+
+y_pred = lassoCV.predict(X_val_std);
+
+#Calculating prediction error
+error = mean_absolute_error(y_true,y_pred);
+print "Mean absolute error: " + str(error) + ", best is zero";
+
+error = mean_squared_error(y_true, y_pred) 
+print "Mean squared error: " + str(error) + ", best is zero";
+
+#=========================Elastic Net ====================================
+print "================Elastic Net ===================="	
+
+enet = linear_model.ElasticNetCV(l1_ratio = [.1, .5, .7, .9, .95, .99, 1] ,alphas=[0.01,0.1,0.3,0.6, 1.0,3.0,6.0, 10.0] );
+
+enet.fit(X_std,y);
+print "Used alpha: " + str(enet.alpha_);
+print "Used l1_ratio: " + str(enet.l1_ratio_);
+
+training_error = enet.score(X_std,y);
+print "Training error = " + str(training_error) + ", best is 1.0";
+
+test_error = enet.score(X_val_std,y_true );
+print "Test error = " + str(test_error) + ", best is 1.0";
+
+y_pred = enet.predict(X_val_std);
+
+#Calculating prediction error
+error = mean_absolute_error(y_true,y_pred);
+print "Mean absolute error: " + str(error) + ", best is zero";
+
+error = mean_squared_error(y_true, y_pred) 
+print "Mean squared error: " + str(error) + ", best is zero";
+
 
 #=======================Support Vector Regression=============================
 print "================Support Vector Regression===================="
 svmR = svm.SVR();
 
 svmR.fit(X_std,y);
+
+training_error = svmR.score(X_std,y);
+print "Training error = " + str(training_error) + ", best is 1.0";
+
+test_error = svmR.score(X_val_std,y_true );
+print "Test error = " + str(test_error) + ", best is 1.0";
 
 y_pred = svmR.predict(X_val_std);
 #print y_pred
@@ -99,8 +173,6 @@ print "Mean absolute error: " + str(error) + ", best is zero";
 error = mean_squared_error(y_true, y_pred) 
 print "Mean squared error: " + str(error) + ", best is zero";
 
-error = r2_score(y_true, y_pred)
-print "R^2 Error: " + str(error) + ", best is 1.0";
-
-
+#accuracy = np.mean( np.divide(y_pred,y_true) )
+#print accuracy
 
