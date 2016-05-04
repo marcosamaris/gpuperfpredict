cdb50798ffca4440af499dad7704d066e6abcee4
dyabmo <muhamed.dyab@gmail.com>
2016-05-04 18:21:16 +0200

    Added possibility to choose experiment, added grid search for choosing hyperparameters

--- ML-model/prediction.py
+++ ML-model/prediction.py
@@ -1,109 +1,80 @@
 import pandas as pd
 import numpy as np
-from sklearn import datasets
-from sklearn import linear_model
-from sklearn import preprocessing
+from sklearn import preprocessing, linear_model, svm, grid_search
 from sklearn.ensemble.forest import RandomForestRegressor
-from sklearn.metrics import mean_absolute_error
-from sklearn.metrics import mean_squared_error
-from sklearn.metrics import r2_score
-from sklearn import svm
+from sklearn.metrics import mean_absolute_error, mean_squared_error, r2_score, make_scorer
 import logging
 from pandas import DataFrame
 
 #TODO: check negatively predicted values
-#TODO: use different linear models
 
 #Gradient tree boosting 
 #http://scikit-learn.org/stable/modules/classes.html#module-sklearn.ensemble
 
-logging.basicConfig(filename='prediction_results.log',filemode='w',level=logging.INFO)
+experiment_type = "compute"
+#options: "compute","classical"
+
+#Output folder name
+outputFolder = ""
+datasetFolder = ""
+
+#Output folder name is set according to experiment type
+if experiment_type == "compute": 
+	datasetFolder = "datasets/with_compute_features/"
+	outputFolder = "prediction_results/with_compute_features/"
+elif experiment_type == "classical":
+	datasetFolder = "datasets/classical/"
+	outputFolder = "prediction_results/classical/"
+
+logging.basicConfig(filename=outputFolder+'prediction_results.log',filemode='w',level=logging.INFO)
 
 #========================Preparing the data===================================#
 
 #Import all dataset
 roundNumber = 5;
-architecuteList = pd.Series(["Kepler","Maxwell"]);
-appIncludeList = pd.Series(["matMul","dotProd","matrix_sum","subSeqMax","vectorAdd"]);
+architecuteList = pd.Series(["Maxwell","Kepler"]);
+appIncludeList = pd.Series(["matrix_sum","matMul","dotProd","subSeqMax","vectorAdd"]);
 
 for i in range(0,appIncludeList.size):
 	
-	dataset = np.genfromtxt('dataset_'+appIncludeList[i]+'.csv', dtype=float, delimiter=',', skip_header =1);
+	dataset = np.genfromtxt(datasetFolder+'dataset_'+appIncludeList[i]+'.csv', dtype=float, delimiter=',', skip_header =1);
 	print dataset.shape;
 
 	#Calculate: number of samples,features, output index, training set size ..
 	featureStart = 1;
-	samplesStart = 0;
-	samplesCount = dataset.shape[0];
 	columnsCount = dataset.shape[1];
 	featuresCount = dataset.shape[1]-1;
 	outputIndex = dataset.shape[1]-1;
-
-	trainingSetCount = int(80 * samplesCount /100);
 	
 	for j in range(0,architecuteList.size):	
 		
 		shape = pd.read_csv("result_shape.csv");
 		shape=shape.set_index('Type');
 
-		if (appIncludeList[i] == "matMul" and architecuteList[j]== "Kepler"):
-			samplesStart = 0; 
-			trainingSetCount =375;
-			samplesCount = 439;
+		trainingGPUs=""; 
+		testGPUs=""
 
-		if (appIncludeList[i] == "matMul" and architecuteList[j]== "Maxwell"):
-			samplesStart = 440; 
-			trainingSetCount =870;
-			samplesCount = 967;
+		#for generic experiment: decide which GPUs are going to be used for training, and which for testing, according to architecture	
+		if (architecuteList[j]== "Kepler"):
+			trainingGPUs = dataset[:,0] < 4;
+			testGPUs = dataset[:,0] == 4;
 
-		if (appIncludeList[i] == "dotProd" and architecuteList[j]== "Kepler"):
-			samplesStart = 0; 
-			trainingSetCount =274;
-			samplesCount = 342;
+		elif(architecuteList[j]== "Maxwell"):
+			trainingGPUs = np.logical_and(dataset[:,0] > 4, dataset[:,0] < 9);
+			testGPUs = dataset[:,0] == 9;
+			
 
-		if (appIncludeList[i] == "dotProd" and architecuteList[j]== "Maxwell"):
-			samplesStart = 343; 
-			trainingSetCount =619;
-			samplesCount = 652;
+		#Training set features (inputs)
+		X = dataset[trainingGPUs,featureStart:featuresCount];
+		print X
+		
+		#Training set outputs
+		y = dataset[trainingGPUs,outputIndex];
+		print y
 
-		if (appIncludeList[i] == "matrix_sum" and architecuteList[j]== "Kepler"):
-			samplesStart = 0; 
-			trainingSetCount =505;
-			samplesCount = 630;
-
-		if (appIncludeList[i] == "matrix_sum" and architecuteList[j]== "Maxwell"):
-			samplesStart = 631; 
-			trainingSetCount =1135;
-			samplesCount = 1260;
-
-		if (appIncludeList[i] == "subSeqMax" and architecuteList[j]== "Kepler"):
-			samplesStart = 0; 
-			trainingSetCount =277;
-			samplesCount = 345;
-
-		if (appIncludeList[i] == "subSeqMax" and architecuteList[j]== "Maxwell"):
-			samplesStart = 346; 
-			trainingSetCount =622;
-			samplesCount = 684;
-
-		if (appIncludeList[i] == "vectorAdd" and architecuteList[j]== "Kepler"):
-			samplesStart = 0; 
-			trainingSetCount =253;
-			samplesCount = 321;
-
-		if (appIncludeList[i] == "vectorAdd" and architecuteList[j]== "Maxwell"):
-			samplesStart = 322; 
-			trainingSetCount =598;
-			samplesCount = 621;
-
-		print trainingSetCount
-		#For training set: separate the feature set from the target attributes
-		X = dataset[samplesStart:trainingSetCount,featureStart:featuresCount]; # last one not included
-		y = dataset[samplesStart:trainingSetCount,outputIndex];
-
-		#True Output values that will be used in calcuating the accuracy of prediction
-		y_true = dataset[trainingSetCount+1:samplesCount,outputIndex]
-		print y_true
+		#True output values for test set
+		y_true = dataset[testGPUs,outputIndex]
+		print y_true	
 
 		#Scale values with mean = zero and standard deviation =1
 		std_scale = preprocessing.StandardScaler().fit(X)
@@ -111,9 +82,16 @@ for i in range(0,appIncludeList.size):
 		#print X_std;
 
 		#Scale test set
-		X_val = dataset[trainingSetCount+1:samplesCount,featureStart:featuresCount];
+		X_val = dataset[testGPUs,featureStart:featuresCount];
 		X_val_std = std_scale.transform(X_val)
 		#print X_val_std;
+
+		logging.info("======================"+appIncludeList[i]+"  " +architecuteList[j]);
+		logging.info("Training set size: "+str(X.shape[0]) );
+		logging.info("Test set size: "+str(X_val.shape[0]) );
+
+		def calculateAccuracy(y_true,y_pred):
+			return np.mean( np.divide(y_pred,y_true) );
 
 		def printErrors(lr,modelType):
 			training_error = lr.score(X_std,y);
@@ -137,19 +115,27 @@ for i in range(0,appIncludeList.size):
 			print "Mean squared error: " + str(error) + ", best is zero";
 			shape['Mean squared error'][modelType] = error;
 
-			accuracy = np.mean( np.divide(y_pred,y_true) ) * 100
+			accuracy =calculateAccuracy(y_true,y_pred);
 			print accuracy
 			shape['Accuracy'][modelType] = accuracy 
 
-			#print y_pred;
+			parameterString = "";
+			if (modelType == "random_forest_regressor" or modelType == "support_vector_regression" ):
+				parameterString = lr.best_params_
+			elif (modelType == "elastic_net"):
+				parameterString = "Alpha: " + str(lr.alpha_) + ", l1_ratio: " + str(lr.l1_ratio_);
+			elif (modelType == "ordinary" ):
+				parameterString = "N/A";
+			elif (modelType == "lasso" or modelType == "ridge"):
+				parameterString = "Alpha: " + str(lr.alpha_);
+				
+			shape['Parameters'][modelType] = parameterString;
 		   	
 			return
 
 		print "======================Application: "+appIncludeList[i];
-		logging.info("======================Application: "+appIncludeList[i]);
 		#=====================Ordinary Least Squares Linear model======================================
 		print "=====================Ordinary Least Squares================"
-		logging.info("=====================Ordinary Least Squares================" );
 		#Training phase
 		lr = linear_model.LinearRegression();
 		lr.fit(X_std,y);
@@ -161,7 +147,6 @@ for i in range(0,appIncludeList.size):
 
 		#===================Ridge Regression==========================================
 		print "=======================Ridge Regression=================="
-		logging.info("=======================Ridge Regression==================");
 		#Try different regularization parameters
 		ridgeCV = linear_model.RidgeCV(alphas=[0.01,0.1,0.3,0.6, 1.0,3.0,6.0, 10.0])
 
@@ -175,7 +160,6 @@ for i in range(0,appIncludeList.size):
 		#=========================LASSO ==============================
 
 		print "=======================LASSO Regression=================="
-		logging.info("=======================LASSO Regression==================");
 		#Try different regularization parameters
 		lassoCV = linear_model.LassoCV(alphas=[0.01,0.1,0.3,0.6, 1.0,3.0,6.0, 10.0])
 
@@ -187,37 +171,46 @@ for i in range(0,appIncludeList.size):
 		printErrors(lassoCV,"lasso");
 		#=========================Elastic Net ====================================
 		print "================Elastic Net ===================="	
-		logging.info("================Elastic Net ====================");
 		enet = linear_model.ElasticNetCV(l1_ratio = [.1, .5, .7, .9, .95, .99, 1] ,alphas=[0.01,0.1,0.3,0.6, 1.0,3.0,6.0, 10.0] );
 
 		enet.fit(X_std,y);
-		print "Used alpha: " + str(enet.alpha_);
-		print "Used l1_ratio: " + str(enet.l1_ratio_);
 
 		y_pred = enet.predict(X_val_std);
 
 		printErrors(enet,"elastic_net");
 		#=======================Support Vector Regression=============================
 		print "================Support Vector Regression===================="
-		logging.info("================Support Vector Regression===================="); 
-		svmR = svm.SVR();
+		
+		parameters = [ { 'kernel':['poly'],'C':[0.01,0.1,0.3,0.6, 1.0,3.0,6.0, 10.0], 'degree' :[1,2,3]  },
+			       {  'kernel':['linear'],'C':[0.01,0.1,0.3,0.6, 1.0,3.0,6.0, 10.0] }, 
+			       {  'kernel':['rbf'],'C':[0.01,0.1,0.3,0.6, 1.0,3.0,6.0, 10.0]} ]
+						
+		svr = svm.SVR();
 
-		svmR.fit(X_std,y);
+		clf = grid_search.GridSearchCV(svr, parameters)		
 
-		y_pred = svmR.predict(X_val_std);
+		clf.fit(X_std,y);
 
-		printErrors(svmR,"support_vector_regression");
+		y_pred = clf.predict(X_val_std);
+		
+		printErrors(clf,"support_vector_regression");
 
 		#====================Random Forest Regressor==========================
 		print "=========Random Forest Regressor==========="
-		logging.info("=========Random Forest Regressor===========");
-		randomForest = RandomForestRegressor(n_estimators=10);
 
-		randomForest.fit(X_std,y);
+		parameters = { 'n_estimators':[2, 4, 8, 16, 32, 64, 128, 256, 512, 1024], 'max_features':( 'auto','sqrt','log2' ) }
+		randomForest = RandomForestRegressor();
+	
+		#Pass a custom accuracy scorer to GridSearchCV, how to tell that 1 is the best ?!
+		#accuracyScorer = make_scorer (calculateAccuracy );
+ 		#scoring=accuracyScorer
+		
+		clf = grid_search.GridSearchCV(randomForest, parameters);
+		clf.fit(X_std,y);
 
-		y_pred = randomForest.predict(X_val_std);
+		y_pred = clf.predict(X_val_std);
+		
+		printErrors(clf,"random_forest_regressor");
 
-		printErrors(randomForest,"random_forest_regressor");
-
-		shape.to_csv("results_"+appIncludeList[i]+"_"+architecuteList[j]+".csv");
+		shape.to_csv(outputFolder+"results_"+appIncludeList[i]+"_"+architecuteList[j]+".csv");
 
