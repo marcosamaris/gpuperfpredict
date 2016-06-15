7081091cdb4e728386155b2f93911b5e0f4f8e25
dyabmo <muhamed.dyab@gmail.com>
2016-06-15 16:59:31 +0200

    Script for performing prediction for random forest and performing plots(not there yet)

--- ML-model/prediction_plots.py
+++ ML-model/prediction_plots.py
@@ -6,39 +6,11 @@ from sklearn.metrics import mean_absolute_error, mean_squared_error, r2_score, m
 import logging
 from pandas import DataFrame
 from scipy import stats
-
-#TODO: check negatively predicted values
-
-#Gradient tree boosting 
-#http://scikit-learn.org/stable/modules/classes.html#module-sklearn.ensemble
-
-onlyUseRandomForest = True
-
-experiment_type = "compute_new"
-#options: "compute","classical","compute_new"
+import matplotlib.pyplot as plt
 
 #Output folder name
-outputFolder = ""
-datasetFolder = ""
-
-#Output folder name is set according to experiment type
-if experiment_type == "compute_new": 
-	datasetFolder = "datasets/with_compute_features_new/"
-	outputFolder = "prediction_results/with_compute_features_new/"
-elif experiment_type == "compute": 
-	datasetFolder = "datasets/with_compute_features/"
-	outputFolder = "prediction_results/with_compute_features/"
-elif experiment_type == "classical":
-	datasetFolder = "datasets/classical/"
-	outputFolder = "prediction_results/classical/"
-
-if onlyUseRandomForest:
-	outputFolder = "prediction_results/RandomForestShuffle/"
-
-logging.basicConfig(filename=outputFolder+'prediction_results.log',filemode='w',level=logging.INFO)
-
-shape = pd.read_csv("result_shape.csv");
-shape=shape.set_index('Type');
+outputFolder = "prediction_results/RandomForestShuffle/"
+datasetFolder = "datasets/with_compute_features_new/"
 
 randomForestResutls = pd.DataFrame(index=range(0,30) , columns=['Training error','Test error','Mean absolute error','Mean squared error','Accuracy','Mean Absolute Percentage Accuracy'] );
 
@@ -77,15 +49,59 @@ def printErrorRandomForest( lr,k):
 
 	return
 
+def boxPlotKepler(all_data,arch):
+
+	fig, axes = plt.subplots(nrows=1, ncols=5, figsize=(12, 5))
+	
+	axes[0].set_xlabel('matMul')
+	axes[1].set_xlabel('matrix_sum')
+	axes[2].set_xlabel('dotProd')
+	axes[3].set_xlabel('subSeqMax')
+	axes[4].set_xlabel('vectorAdd')
+
+	# rectangular box plot
+	bplot1 = axes[0].boxplot(all_data,
+		                 vert=True,   # vertical box aligmnent
+		                 patch_artist=True)   # fill with color
+
+	# notch shape box plot
+	bplot2 = axes[1].boxplot(all_data,
+		                 notch=True,  # notch shape
+		                 vert=True,   # vertical box aligmnent
+		                 patch_artist=True)   # fill with color
+
+	# fill with colors
+	colors = ['pink', 'lightblue', 'lightgreen','red','yellow','black']
+	for bplot in (bplot1, bplot2):
+	    for patch, color in zip(bplot['boxes'], colors):
+		patch.set_facecolor(color)
+
+	# adding horizontal grid lines
+	for ax in axes:
+	    ax.yaxis.grid(True)
+	    ax.set_xticks([y+1 for y in range(len(all_data))], )
+	    ax.set_ylabel('Accuracy')
+
+	if arch == "Kepler":
+		xticklabels=['GTX-680', 'Tesla-K40', 'Tesla-K20', 'TitanBlack','Titan','Quadro']
+
+	if arch == "Maxwell":
+		xticklabels=['GTX-750', 'TitanX', 'GTX-980', 'GTX-970']
+
+	# add x-tick labels
+	plt.setp(axes, xticks=[y+1 for y in range(len(all_data))],
+		 xticklabels=xticklabels)
+
+	plt.show()
+
+
 #========================Preparing the data===================================#
 
 #Import all dataset
 roundNumber = 5;
 architecuteList = pd.Series(["Kepler","Maxwell"]);
+architecuteList = pd.Series(["Kepler"]);
 appIncludeList = pd.Series(["matMul","matrix_sum","dotProd","subSeqMax","vectorAdd"]);
-
-#appIncludeList = pd.Series(["matMul_gpu", "matMul_gpu_sharedmem", "matMul_gpu_uncoalesced", "matMul_gpu_sharedmem_uncoalesced", 
-#			   "dotProd","matrix_sum_coalesced","matrix_sum_normal", "subSeqMax", "vectorAdd"]);
 
 for i in range(0,appIncludeList.size):
 	
@@ -101,82 +117,21 @@ for i in range(0,appIncludeList.size):
 
 		trainingGPUs=""; 
 		testGPUs=""
-			
-		#Kepler Architecture Group
-		gpu1= dataset[:,0] == 1;
-		gpu2= dataset[:,0] == 2;
-		gpu3= dataset[:,0] == 3;
-		gpu4= dataset[:,0] == 4;
-		gpu5= dataset[:,0] == 5;
-		gpu6= dataset[:,0] == 6;
-
-		#Maxwell Architecture Group
-		gpu7= dataset[:,0] == 7;
-		gpu8= dataset[:,0] == 8;
-		gpu9= dataset[:,0] == 9;
-		gpu10= dataset[:,0] == 10;
 		
 		rangeForLoop =0;
 		if (architecuteList[j]== "Kepler"):
 			rangeForLoop = range (1,7)
+
+			all_gpu_avg_accuracy=0;
+
+			for m in rangeForLoop:
 	
-		elif(architecuteList[j]== "Maxwell"):
-			rangeForLoop = range (7,11)
-
-		for m in rangeForLoop:
-		
-			if m==1:
-				trainingGPUs = np.logical_or.reduce( (gpu2,gpu3,gpu4,gpu5,gpu6));
-				testGPUs = gpu1;
-
-			elif m==2:
-				trainingGPUs = np.logical_or.reduce( (gpu1,gpu3,gpu4,gpu5,gpu6));
-				testGPUs = gpu2;
-
-			elif m==3:
-				trainingGPUs = np.logical_or.reduce(  (gpu1,gpu2,gpu4,gpu5,gpu6));
-				testGPUs = gpu3;
-
-			elif m==4:
-				trainingGPUs = np.logical_or.reduce(  (gpu1,gpu2,gpu3,gpu5,gpu6));
-				testGPUs = gpu4;
-
-			elif m==5:
-				trainingGPUs = np.logical_or.reduce((gpu1,gpu2,gpu3,gpu4,gpu6));
-				testGPUs = gpu5;
-		
-			elif m==6:
-				trainingGPUs = np.logical_or.reduce( (gpu1,gpu2,gpu3,gpu4,gpu5));
-				testGPUs = gpu6;
-			
-			elif m==7:
-				trainingGPUs = np.logical_or.reduce((gpu8,gpu9,gpu10));
-				testGPUs = gpu7;
-
-			elif m==8:
-				trainingGPUs = np.logical_or.reduce((gpu7,gpu9,gpu10));
-				testGPUs = gpu8;
-
-			elif m==9:
-				trainingGPUs = np.logical_or.reduce((gpu7,gpu8,gpu10));
-				testGPUs = gpu9;
-
-			elif m==10:
-				trainingGPUs = np.logical_or.reduce((gpu7,gpu8,gpu9));
-				testGPUs = gpu10;
+				trainingGPUs = dataset[:,0] != m;
+				testGPUs = dataset[:,0] == m;
 	
-			# For calculating accuracy and abs percentage deviation for each sample
-			accuracy_sample_temp = 0
-			abs_percent_dev_temp = 0
-			
-			# For calculating accuracy and abs percentage deviation for each sample
-			accuracy_sample_temp = 0
-			abs_percent_dev_temp = 0
-
-			for k in range (0,10):
 				#Training set features (inputs)
 				X = dataset[trainingGPUs,featureStart:featuresCount];
-		
+
 				#Training set outputs
 				y = dataset[trainingGPUs,outputIndex];
 
@@ -192,31 +147,110 @@ for i in range(0,appIncludeList.size):
 				X_val = dataset[testGPUs,featureStart:featuresCount];
 				X_val_std = std_scale.transform(X_val)
 				#print X_val_std;
-
-				logging.info("======================"+appIncludeList[i]+"  " +architecuteList[j]);
-				logging.info("Training set size: "+str(X.shape[0]) );
-				logging.info("Test set size: "+str(X_val.shape[0]) );
-
-				#print "======================Application: "+appIncludeList[i];
-
-				#print "=========Random Forest Regressor==========="
-
-				#parameters = { 'n_estimators':[2, 4, 8, 16, 32, 64, 128, 256, 512, 1024], 'max_features':( 'auto','sqrt','log2' ) }
-				#randomForest = RandomForestRegressor( n_estimators = 16 );
-				clf  = RandomForestRegressor( n_estimators = 16 );
 	
-				#clf = grid_search.GridSearchCV(randomForest, parameters);
-				clf.fit(X_std,y);
+				#number of rows = number of GPUs, number of columns = number of samples
+				all_gpu_avg_accuracy = np.zeros([6,len(y_true)])			
 
-				y_pred = clf.predict(X_val_std);
-				
-				#==================Processing of Results==================				
-				accuracy_sample =  np.divide(y_pred,y_true);
-				abs_percent_dev =  np.absolute( np.divide( (y_true - y_pred),y_true )  );
-				
+				# For calculating the average accuracy and abs percentage deviation for each sample after all runs
+				accuracy_sample = np.zeros([10,len(y_true)])
+				abs_percent_dev = np.zeros([10,len(y_true)])
 
-				printErrorRandomForest(clf,k);
-				randomForestResutls.to_csv(outputFolder+"results_"+appIncludeList[i]+"_"+architecuteList[j]+"_RandomForestResults_"+str(m)+".csv");
+				for k in range (0,10):
+
+					clf  = RandomForestRegressor( n_estimators = 16 );
+					clf.fit(X_std,y);
+
+					y_pred = clf.predict(X_val_std);
+				
+					#==================Processing of Results==================				
+					accuracy_sample_temp =  np.divide(y_pred,y_true);
+					abs_percent_dev_temp =  np.absolute( np.divide( (y_true - y_pred),y_true )  );
+
+					accuracy_sample[k,:] = accuracy_sample_temp
+					abs_percent_dev[k,:] =abs_percent_dev_temp
+				
+					printErrorRandomForest(clf,k);
+					randomForestResutls.to_csv(outputFolder+"results_"+appIncludeList[i]+"_"+architecuteList[j]+"_RandomForestResults_"+str(m)+".csv");
+			
+				# Average accuracy for each sample after 10 runs
+				average_accuracy = np.mean (accuracy_sample, axis = 0)
+				average_abs_percent_dev = np.mean (abs_percent_dev, axis = 0)
+			
+				#make a list
+				x = [ average_accuracy, average_abs_percent_dev ]
+				all_gpu_avg_accuracy[m,:average_accuracy]
+
+			boxPlotKepler(x,"Kepler")
+
+	
+		elif(architecuteList[j]== "Maxwell"):
+			rangeForLoop = range (7,11)
+
+			all_gpu_avg_accuracy=0;
+
+			for m in rangeForLoop:
+	
+				trainingGPUs = dataset[:,0] != m;
+				testGPUs = dataset[:,0] == m;
+	
+				#Training set features (inputs)
+				X = dataset[trainingGPUs,featureStart:featuresCount];
+
+				#Training set outputs
+				y = dataset[trainingGPUs,outputIndex];
+
+				#True output values for test set
+				y_true = dataset[testGPUs,outputIndex]
+
+				#Scale values with mean = zero and standard deviation =1
+				std_scale = preprocessing.StandardScaler().fit(X)
+				X_std = std_scale.transform(X)
+				#print X_std;
+
+				#Scale test set
+				X_val = dataset[testGPUs,featureStart:featuresCount];
+				X_val_std = std_scale.transform(X_val)
+				#print X_val_std;
+	
+				#number of rows = number of GPUs, number of columns = number of samples
+				all_gpu_avg_accuracy = np.zeros([4,len(y_true)])			
+
+				# For calculating the average accuracy and abs percentage deviation for each sample after all runs
+				accuracy_sample = np.zeros([10,len(y_true)])
+				abs_percent_dev = np.zeros([10,len(y_true)])
+
+				for k in range (0,10):
+
+					clf  = RandomForestRegressor( n_estimators = 16 );
+					clf.fit(X_std,y);
+
+					y_pred = clf.predict(X_val_std);
+				
+					#==================Processing of Results==================				
+					accuracy_sample_temp =  np.divide(y_pred,y_true);
+					abs_percent_dev_temp =  np.absolute( np.divide( (y_true - y_pred),y_true )  );
+
+					accuracy_sample[k,:] = accuracy_sample_temp
+					abs_percent_dev[k,:] =abs_percent_dev_temp
+				
+					printErrorRandomForest(clf,k);
+					randomForestResutls.to_csv(outputFolder+"results_"+appIncludeList[i]+"_"+architecuteList[j]+"_RandomForestResults_"+str(m)+".csv");
+			
+				# Average accuracy for each sample after 10 runs
+				average_accuracy = np.mean (accuracy_sample, axis = 0)
+				average_abs_percent_dev = np.mean (abs_percent_dev, axis = 0)
+			
+				#make a list
+				x = [ average_accuracy, average_abs_percent_dev ]
+				#x = average_accuracy;
+				boxPlot(x,"Maxwell")
+				#plt.boxplot(x)
+				#plt.show()
+				#print average_accuracy
+				#print average_abs_percent_dev
+		
+		
+
 
 				
 
